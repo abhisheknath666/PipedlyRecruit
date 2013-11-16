e70fbf6349d75c962f1cfda28f14bbbc784d39e6
Abhishek Nath <illidan.elf@gmail.com>
2013-11-15 22:01:58 -0800

    Fix access token retrieval step 2

--- pipedlyapp/urls.py
+++ pipedlyapp/urls.py
@@ -6,5 +6,4 @@ urlpatterns = patterns('',
     url(r'^(?P<poll_id>[0-9]+)/$', views.grab_people, name='grab people'),
     # url(r'^linkedin/?code=(?P<code>[-\w\d\_]+)&state=(?P<state>[-\w\d\_]+)/$', views.on_ln_authentication_response, name='Linkedin authentication response') 
     url(r'^ln/auth/$', views.on_ln_authentication_response, name='Linkedin authentication response')
-    
 )
--- pipedlyapp/views.py
+++ pipedlyapp/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from pipedlyapp.models import Poll
 
-import httplib, urllib
+import httplib, urllib, urllib2
 import hashlib
+
+LN_CLIENT_ID = "755uojkm0y48vy"
+LN_CLIENT_SECRET = "2aXh3DWAJEIYsxbc"
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -17,28 +20,28 @@ def grab_people(request,poll_id):
 
     state = hashlib.sha224("access_token").hexdigest()
     params = urllib.urlencode({ 'response_type' : "code",
-                                'client_id' : "755uojkm0y48vy",
+                                'client_id' : LN_CLIENT_ID,
                                 'state' : state,
                                 'redirect_uri' : 'http://www.abhisheknath.net/ln/auth/' })
-    # connection = httplib.HTTPSConnection("www.linkedin.com")
-    # connection.request("POST", "/uas/oath2/authorization", params)
-    # response = connection.getresponse()
-    # # print response.status, response.reason
-    # data = response.read()
-    # # print data
-    # return response
     url_string = "https://www.linkedin.com/uas/oauth2/authorization?"+params
     return HttpResponseRedirect(url_string)
 
 def on_ln_authentication_response(request):
+    #{"expires_in":5183999,"access_token":"AQXJCs0TreX3u8xMwh8H1926u7QCjhwwTSbgynQ65Qs7WsnTloitTPitrRF2Qczc40n1TeMgQ7Q0nPyJ72EHVqhZ8GColXZ-5j7Gnck_lzSjcv0kaV505U3OKsJqJ-JGMqOInYwYEVYgUoZ2AX1i5MWD1mgZmQ3ryFxXBc7Psi_2nNai1cQ"}
     code = request.GET.get('code')
-    print "Token: "+code
+    print "Code: "+code
     state = request.GET.get('state')
     if(state==hashlib.sha224("access_token").hexdigest()):
         print "State: "+state
     else:
         print "Unknown state"
-    return HttpResponse('success')
 
-
-        #127.0.0.1:8000/polls/ln/auth/?code=AQQjSnDitr3NQfLl8sENB8Dlg0hBPvxDeka7xrfH7ICVYKPW9CUrghXUo-6P12WQMEGjsdWg0lYus5kaxlYFT7-fnGDyWn8q0W_d50VWroMeM1GN3NU&state=ef77eccddf5ecefbb05d2218321937ff3c3de07ba4c9bc2ecae71312
+    params = urllib.urlencode({ 'grant_type':"authorization_code",
+                                'code' : code,
+                                'redirect_uri' : 'http://www.abhisheknath.net/ln/auth/',
+                                'client_id' : LN_CLIENT_ID,
+                                'client_secret' : LN_CLIENT_SECRET })
+    url_string = "https://www.linkedin.com/uas/oauth2/accessToken?"+params
+    print url_string
+    connection = urllib.urlopen(url_string, data="")
+    return HttpResponse(connection.read())
