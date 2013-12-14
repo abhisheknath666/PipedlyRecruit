6b5352d9522d023f21b5b7b6484dd07068000862
Abhishek Nath <illidan.elf@gmail.com>
2013-12-14 10:12:42 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='pipedly',
+      version='0.1',
+      description='Piping service Apis to build new services',
+      author='Abhishek Nath',
+      author_email='illidan.elf@gmail.com',
+      url='https://github.com/abhisheknath666/PipedlyRecruit',
+      platforms = ['Any'],
+      py_modules = ['scrappinghub'],
+      install_requires = ['requests','django','MySQL-python']
+      )
