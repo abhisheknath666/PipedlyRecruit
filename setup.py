4f3ee306fdf539584e681e5fa171658c9ccab5de
Abhishek Nath <illidan.elf@gmail.com>
2013-12-14 10:43:46 -0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='pipedly',
       author_email='illidan.elf@gmail.com',
       url='https://github.com/abhisheknath666/PipedlyRecruit',
       platforms = ['Any'],
-      py_modules = ['scrappinghub'],
+      py_modules = ['lib.scrapinghub.scrapinghub'],
       install_requires = ['requests','django','MySQL-python']
       )
