ef7960aff351d3cdb5ef2999c14c4020121b2454
Abhishek Nath <illidan.elf@gmail.com>
2013-12-17 20:54:29 -0800

    Fix settings.py and requirements.txt. Add .gitignore

--- pipedly/settings.py
+++ pipedly/settings.py
@@ -10,18 +10,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pipedly',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -158,8 +146,16 @@ LOGGING = {
 }
 
 # Parse database configuration from $DATABASE_URL
+
+DATABASES = {
+}
+
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+import os
+if not os.environ.has_key('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(default='mysql://root@localhost:3306/pipedly')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
