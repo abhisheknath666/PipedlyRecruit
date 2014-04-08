55dac0e811061340603ad19fb9eb987b4753a57f
Abhishek Nath <illidan.elf@gmail.com>
2014-04-07 22:20:37 -0700

    Add issues over time

--- pipedlyapp/underworld_dashboard.py
+++ pipedlyapp/underworld_dashboard.py
@@ -52,12 +52,60 @@ having count(*) > 3
 order by count(*) desc
  limit 10'''
 
+issues_over_time_query = '''select
+
+c.date as date
+
+, count(*)
+
+from
+
+ pipedlyapp_semantriatheme a
+
+,pipedlyapp_semantriaitem b
+
+, pipedlyapp_scrapinghubitem c
+
+where
+
+a.document_id_id= b.id
+
+and
+
+b.document_id_id=c.id
+
+and
+
+a.title not like '%20%'
+
+ and
+
+ b.sentiment_polarity = 1
+
+ and
+
+ char_length(a.title)>2
+
+ and
+
+c.forum_post similar to '(%quit|%abandon%|%give up%|%discontinue%)'
+
+group by c.date
+
+having count(*) > 3
+
+order by count(*) desc
+
+ limit 10'''
+
 def show_dashboard(request, name=''):
     render_data = {}
     churn_data = get_churn_data()
     crash_data = get_crash_data()
+    issues_overtime_data = get_issues_over_time()
     render_data.update(churn_data)
     render_data.update(crash_data)
+    render_data.update(issues_overtime_data)
     return render(request, 'pipedly/underworld_dashboard.html', render_data)
 
 def get_churn_data():
@@ -74,8 +122,19 @@ def get_crash_data():
     total = reduce(lambda x,y: x+y, values)
     percentages = map(lambda p: p*100.0/total, values)
     labels = [ label[0] for label in rows ]
-    return {"crash_percentages":json.dumps(percentages), "crash_labels":json.dumps(labels)}    
+    return {"crash_percentages":json.dumps(percentages), "crash_labels":json.dumps(labels)}
 
+def get_issues_over_time():
+    rows = my_custom_sql(issues_over_time_query)
+    counts = [ tup[1] for tup in rows]
+    dates = [ label[0] for label in rows ]
+    dthandler = lambda obj: (
+    obj.isoformat()
+    if isinstance(obj, datetime.datetime)
+    or isinstance(obj, datetime.date)
+        else "")
+    return {"counts":json.dumps(counts), "dates":json.dumps(dates, default=dthandler)}
+    
 def my_custom_sql(query):
     cursor = connection.cursor()
 
