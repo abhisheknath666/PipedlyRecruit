43fdd559354cf176cc28ecfc1b07e6a232023da9
ksriraman <karthicks@gmail.com>
2014-04-14 20:58:43 -0700

    Added sentiment by theme chart

--- pipedlyapp/underworld_dashboard.py
+++ pipedlyapp/underworld_dashboard.py
@@ -74,16 +74,39 @@ c.forum_post similar to '(%quit|%abandon%|%give up%|%discontinue%)'
 group by c.date
 having count(*) > 3
 order by count(*) desc
- limit 10'''
+limit 10'''
+ 
+sentiment_by_theme_query = '''select * from crosstab( 'select
+b.theme as Theme, 
+case 
+when a.sentiment_polarity = 2 then ''Neutral'' 
+when a.sentiment_polarity = 1 then ''Negative'' 
+WHEN a.sentiment_polarity = 0 then ''Positive'' 
+end as Sentiment, 
+count(*) as Count
+from pipedlyapp_semantriaentity a,
+    (select title as theme from pipedlyapp_semantriatheme
+  group by title having count(*) >100) b, 
+  pipedlyapp_semantriatheme c
+    where a.document_id_id=c.document_id_id 
+    and b.theme=c.title
+    and c.title not like ''%Blog%''
+group by
+    b.theme,
+    a.sentiment_polarity
+  order by theme
+   limit 30') as ct(Theme varchar(255), Neutral bigint, Negative bigint, Positive bigint);'''
 
 def show_dashboard(request, name=''):
     render_data = {}
     churn_data = get_churn_data()
     crash_data = get_crash_data()
     issues_overtime_data = get_issues_over_time()
+    sentiment_by_theme = get_sentiment_by_theme()
     render_data.update(churn_data)
     render_data.update(crash_data)
     render_data.update(issues_overtime_data)
+    render_data.update(sentiment_by_theme)
     return render(request, 'pipedly/underworld_dashboard.html', render_data)
 
 def get_churn_data():
@@ -112,6 +135,17 @@ def get_issues_over_time():
     or isinstance(obj, datetime.date)
         else "")
     return {"counts":json.dumps(counts), "dates":json.dumps(dates, default=dthandler)}
+    
+def get_sentiment_by_theme():
+    rows = my_custom_sql(sentiment_by_theme_query)
+    theme = [ tup[0] for tup in rows]
+    neutral = [ tup[1] for tup in rows]
+    #neutral = [ 1 for tup in rows]
+    negative = [ tup[2] for tup in rows]
+    #negative = [ 1 for tup in rows]
+    positive = [ tup[3] for tup in rows]
+    #positive = [ 1 for tup in rows]
+    return {"theme":json.dumps(theme), "neutral":json.dumps(neutral), "negative":json.dumps(negative), "positive":json.dumps(positive)}
     
 def my_custom_sql(query):
     cursor = connection.cursor()
